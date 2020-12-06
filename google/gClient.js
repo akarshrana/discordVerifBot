918e8031644d5627dbff35ffd40adcb113fd142c
has12zen <57583693+has12zen@users.noreply.github.com>
2020-12-06 21:38:45 +0530

    Made production changes

--- google/gClient.js
+++ google/gClient.js
@@ -1,31 +1,31 @@
 const { gsrun } = require('./gsRun');
-const { google } = require("googleapis");
-// const { config } = require("dotenv");
-// config({ path: __dirname + "/.env" });
-const keys = require("../data/keys.json");
+const { google } = require('googleapis');
 
+// const keys = require('../data/keys.json');
+const keysEnvVar = process.env.CREDS;
+const keys = JSON.parse(keysEnvVar); // https://stackoverflow.com/questions/57334178/how-to-use-env-to-store-a-json-key-for-use-with-jwt
 // const googleClient = new google.auth.JWT(
 //     process.env.CLIENT_EMAIL,
 //     null,
 //     process.env.PRIVATE_KEY,
 //     ["https://www.googleapis.com/auth/spreadsheets"]
 // );
-
+// console.log(keys);
 const googleClient = new google.auth.JWT(
-    keys.client_email,
-    null,
-    keys.private_key,
-    ["https://www.googleapis.com/auth/spreadsheets"]
+  keys.client_email,
+  null,
+  keys.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets']
 );
 
 exports.gClientAuthorize = (gClient) => {
-    console.log('Authorizing...');
-    console.log(keys.client_email, keys.private_key);
-    // console.log(process.env.CLIENT_EMAIL, process.env.PRIVATE_KEY);
-    gClient.authorize((err) => {
-        if (err) throw (err);
-        console.log('Connected!');
-    });
+  console.log('Authorizing...');
+  //   console.log(k);
+  // console.log(process.env.CLIENT_EMAIL, process.env.PRIVATE_KEY);
+  gClient.authorize((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+  });
 };
 
 exports.gClient = googleClient;
--- index.js
+++ index.js
@@ -1,63 +1,69 @@
-const { Client } = require("discord.js");
+const { Client } = require('discord.js');
+if (process.env.NODE_ENV !== 'production') {
+  const { config } = require('dotenv');
+  config({ path: __dirname + '/.env' });
+}
 const { gClient, gClientAuthorize } = require('./google/gClient');
 const { gsrun } = require('./google/gsRun');
 const botCommands = require('./commands/botCommands');
 const util = require('./utility');
-const { config } = require("dotenv");
+
 const reactChannelId = '752837945165348894';
-config({ path: __dirname + "/.env" });
 
 // Google Authorize + Load Data
 gClientAuthorize(gClient);
 (async () => {
-	data = await gsrun(gClient);
+  data = await gsrun(gClient);
 })();
 
-// Discord Client        
+// Discord Client
 const client = new Client({
-	disableEveryone: true
+  disableEveryone: true,
 });
 
 // Discord Connect
-client.on("ready", () => {
-	console.log(`${client.user.username} ONLINE!`);
-	client.user.setActivity('Fight Club', { type: "WATCHING" });
+client.on('ready', () => {
+  console.log(`${client.user.username} ONLINE!`);
+  client.user.setActivity('Fight Club', { type: 'WATCHING' });
 });
 
 // Bot Listeners
-client.on("message", async message => {
-	if (!message.content.startsWith('!')) return;
+client.on('message', async (message) => {
+  if (!message.content.startsWith('!')) return;
 
-	const fullMessage = message.content.slice(1);
-	const args = fullMessage.split(' ');
-	const command = args.shift();
+  const fullMessage = message.content.slice(1);
+  const args = fullMessage.split(' ');
+  const command = args.shift();
 
-	console.log(`${command} from ${message.author.username}`);
+  console.log(`${command} from ${message.author.username}`);
 
-	if (command === 'refresh') botCommands.refresh(gClient, message).then(newData => { data = newData; });
-	else if (command === 'verif') botCommands.verifyMember(message, data, args);
-	else if (command === 'addRole') message.member.reply('Nothing here check back later');
-	else if (command === "assign") botCommands.assignRole(message, args);
-	else if (command === "18EE") botCommands.ee18(message);
-	else if (command === "sendMessage" && message.author.username === "a_crush") {
-		botCommands.sendMessage(client, message);
-	}
+  if (command === 'refresh')
+    botCommands.refresh(gClient, message).then((newData) => {
+      data = newData;
+    });
+  else if (command === 'verif') botCommands.verifyMember(message, data, args);
+  else if (command === 'addRole')
+    message.member.reply('Nothing here check back later');
+  else if (command === 'assign') botCommands.assignRole(message, args);
+  else if (command === '18EE') botCommands.ee18(message);
+  else if (command === 'sendMessage' && message.author.username === 'a_crush') {
+    botCommands.sendMessage(client, message);
+  }
 });
 
-client.on("messageReactionAdd", (reaction, user) => {
-	if (reaction.message.channel.id === reactChannelId) {
-		console.log("Add");
-		util.handleReaction(reaction, user, true);
-	}
-})
+client.on('messageReactionAdd', (reaction, user) => {
+  if (reaction.message.channel.id === reactChannelId) {
+    console.log('Add');
+    util.handleReaction(reaction, user, true);
+  }
+});
 
-
-client.on("messageReactionRemove", (reaction, user) => {
-	if (reaction.message.channel.id === reactChannelId) {
-		console.log("Remove");
-		util.handleReaction(reaction, user, false);
-	}
-})
+client.on('messageReactionRemove', (reaction, user) => {
+  if (reaction.message.channel.id === reactChannelId) {
+    console.log('Remove');
+    util.handleReaction(reaction, user, false);
+  }
+});
 
 // Discord Client Login
 client.login(process.env.TOKEN);
